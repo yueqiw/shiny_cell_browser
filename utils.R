6a8178b8f943f222e9340c4e6113a4603c536b78
Yueqi Wang <yueqi.wang.pku@gmail.com>
2018-07-15 06:43:27 -0600

    add more arguments to dot plots

--- utils.R
+++ utils.R
@@ -12,14 +12,15 @@ DotPlot2 <- function(
   color_scaling = "zero-one", # or "mean-var"
   size_scaling = "area", # or "radius"
   cols.use = c("grey90", "red"),
+  horizontal = FALSE,
   col.min = -2.5,
   col.max = 2.5,
   dot.min = 0,
   dot.scale = 6,
+  axis.label.size = 15,
   group.by,
-  plot.legend = FALSE,
-  do.return = FALSE,
-  x.lab.rot = FALSE
+  legend.position = "none",
+  do.return = FALSE
 ) {
   color_scaling_types <- c("mean-var", "zero-one")
   if (! color_scaling %in% color_scaling_types) {
@@ -34,6 +35,9 @@ DotPlot2 <- function(
     object <- SetAllIdent(object = object, id = group.by)
   }
 
+  if (horizontal) {
+      genes.plot <- rev(genes.plot)
+  }
   genes.plot <- genes.plot %>% unique()
   genes.plot.exist <- genes.plot[genes.plot %in% rownames(object@data)]
   genes.plot.nonexist <- genes.plot[!genes.plot %in% rownames(object@data)]
@@ -52,19 +56,25 @@ DotPlot2 <- function(
   data.to.plot <- data.to.plot %>%
     group_by(id, genes.plot) %>%
     summarize(
-      avg.exp = mean(expm1(x = expression)),
+      avg.exp.raw = mean(expm1(x = expression)),
       pct.exp = PercentAbove(x = expression, threshold = 0)
     )
   data.to.plot <- data.to.plot %>%
     ungroup() %>%
-    group_by(genes.plot) %>%
-    mutate(avg.exp.scale = scale(x = avg.exp)) %>%
-    mutate(avg.exp.scale = MinMax(
-      data = avg.exp.scale,
-      max = col.max,
-      min = col.min
-    )) %>%
-    mutate(avg.exp.scale.one = avg.exp / max(avg.exp))
+    group_by(genes.plot)
+
+  if (color_scaling == "mean-var") {
+      data.to.plot <- data.to.plot %>%
+        mutate(avg.exp = scale(x = avg.exp.raw)) %>%
+        mutate(avg.exp = MinMax(
+          data = avg.exp,
+          max = col.max,
+          min = col.min
+        ))
+  } else if (color_scaling == "zero-one") {
+      data.to.plot <- data.to.plot %>%
+        mutate(avg.exp = avg.exp.raw / max(avg.exp.raw))
+  }
 
 
   data.to.plot$genes.plot <- factor(
@@ -79,28 +89,31 @@ DotPlot2 <- function(
       scale_func <- scale_radius
   }
 
+  p <- ggplot(data = data.to.plot, mapping = aes(x = genes.plot, y = id)) +
+      geom_point(mapping = aes(size = pct.exp, color = avg.exp)) +
+      scale_func(limits = c(0.001,1), range = c(0, dot.scale))
+
   if (color_scaling == "mean-var") {
-    p <- ggplot(data = data.to.plot, mapping = aes(x = genes.plot, y = id)) +
-        geom_point(mapping = aes(size = pct.exp, color = avg.exp.scale)) +
-        scale_func(limits = c(0.001,1), range = c(0, dot.scale)) +
-        scale_color_gradient(low = cols.use[1], high = cols.use[2])
+    p <- p + scale_color_gradient(low = cols.use[1], high = cols.use[2])
   } else if (color_scaling == "zero-one") {
-    p <- ggplot(data = data.to.plot, mapping = aes(x = genes.plot, y = id)) +
-        geom_point(mapping = aes(size = pct.exp, color = avg.exp.scale.one)) +
-        scale_func(limits = c(0.001,1), range = c(0, dot.scale)) +
-        scale_color_gradient(low = cols.use[1], high = cols.use[2], limits = c(0,1))
+    p <- p + scale_color_gradient(low = cols.use[1], high = cols.use[2], limits = c(0,1))
   } else {
       stop("color scaling error.")
   }
   p <- p +
-    theme(axis.title.x = element_blank(), axis.title.y = element_blank())
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.x = element_text(face="bold", size=axis.label.size),
+          axis.text.y = element_text(face="bold", size=axis.label.size),
+          legend.position = legend.position)
 
-  if (! plot.legend) {
-    p <- p + theme(legend.position = "none")
+  if (horizontal) {
+    p <- p + scale_y_discrete(limits = rev(levels(data.to.plot$id)))
+    p <- p + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  } else {
+    p <- p + coord_flip()
   }
-  if (x.lab.rot) {
-    p <- p + theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
-  }
+
   suppressWarnings(print(p))
   if (do.return) {
     return(p)
